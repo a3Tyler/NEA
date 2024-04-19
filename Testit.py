e62738da35c2c74f025c50aa11d64df48fb3657a
a3Tyler <167545129+a3Tyler@users.noreply.github.com>
2024-04-19 14:31:34 +0000

    First addition

---
+++ Testit.py
@@ -0,0 +1,9 @@
+#This file contains the code for TestIt! (my NEA project)
+
+#Import key functions
+import tkinter
+from tkinter import label, entry, button
+
+#New subroutine called 'Welcome'
+def Welcome():
+    txtWelcome_title = label()
