0b6ff2bcc1615573f103187578623dc15d184501
a3Tyler <167545129+a3Tyler@users.noreply.github.com>
2024-04-19 14:54:14 +0000

    Prototype v1 - Creation - 19/04/2024

--- Testit.py
+++ Testit.py
@@ -1,13 +1,13 @@
-#This file contains the code for TestIt! (my NEA project)
+# This file contains the code for TestIt! (my NEA project)
 
-#Import key functions
+# Import key functions
 import tkinter
 from tkinter import Tk, Label, Entry, Button
 
-#Create a title
+# Create a title
 Title = Tk()
 Title.title("TestIt!")
 
-#New subroutine called 'Welcome'
+# New subroutine called 'Welcome'
 def Welcome():
     txtWelcome_title = tkinter.label(text = "Welcome to TestIt!")
