e6760a0fe0faa4cb0141b9ea26a860386680f008
a3Tyler <167545129+a3Tyler@users.noreply.github.com>
2024-05-09 14:05:21 +0100

    Prototype v1 - Scaling
    Added scaling to widgets
    Problem with text flickering and not scaling

--- Testit.py
+++ Testit.py
@@ -2,12 +2,24 @@
 
 # Import key functions
 import tkinter
-from tkinter import Tk, Label, Entry, Button
+from tkinter import Tk, Label, Entry, Button, CENTER
 
-# Create a title
+# Creates a title
 Title = Tk()
 Title.title("TestIt!")
 
-# New subroutine called 'Welcome'
+# Subroutine that changes the size of text when the window size changes
+def resize(e):
+    global txtWelcome_title
+    size = e.width/10
+    txtWelcome_title.config(font = ("Arial", int(round(e.height/20))))
+
+# Subroutine that displays the Welcome screen
 def Welcome():
-    txtWelcome_title = tkinter.label(text = "Welcome to TestIt!")
+    global txtWelcome_title
+    txtWelcome_title = tkinter.Label(text = "Welcome to TestIt!", font = ("Arial", 21))
+    txtWelcome_title.place(relx = 0.5, rely = 0.05, anchor = CENTER)
+    Title.bind('<Configure>', resize)
+    Title.mainloop()
+
+Welcome()
