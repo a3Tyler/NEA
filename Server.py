b762b4a977340bfb490f0ec985f8ac0cdea8677a
a3Tyler <167545129+a3Tyler@users.noreply.github.com>
2024-05-24 09:09:27 +0100

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,6 +1,7 @@
 # # # # # TESTIT SERVER PROGRAM # # # # #
 import mysql.connector
 
-mydb = mysql.connector.connect(host = "localhost", username = input("Enter username : "), password = input("Enter password : "))
+mydb = mysql.connector.connect(host = "localhost", username = "username", password = "password")
 
 print(mydb)
+# # # # # END OF PROGRAM # # # # #
