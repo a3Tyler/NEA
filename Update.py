d8b1186cfe3046c8feb46030618e3af6b91e30c5
a3Tyler <167545129+a3Tyler@users.noreply.github.com>
2024-06-28 15:55:42 +0100

    Add files via upload

--- Update.py
+++ Update.py
@@ -2,7 +2,7 @@ from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import *
 
-engine = create_engine('sqlite:///user_database.db', pool_pre_ping = True, echo = False)
+engine = create_engine('sqlite:///user_database.db', echo = True)
 
 Base = declarative_base()
 
@@ -11,26 +11,29 @@ Base.metadata.create_all(engine)
 Session = sessionmaker(bind = engine)
 
 class Parent(Base):
-    __tablename__ = 'Parent'
-    
-    # Table fields
-    id : Mapped[int] = mapped_column(primary_key = True)
-    age : Mapped[int] = mapped_column()
-    name : Mapped[str] = mapped_column()
-    children : Mapped[list["Child"]] = mapped_column(ForeignKey("Child.id"))
+    __tablename__ = "parent"
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String, unique = True)
+    child = relationship("Child", back_populates = "parent")
 
 class Child(Base):
-    __tablename__ = 'Child'
-    
-    # Table fields
-    id : Mapped[int] = mapped_column(primary_key = True)
-    age : Mapped[int] = mapped_column()
-    name : Mapped[str] = mapped_column()
-    parent : Mapped["Parent"] = mapped_column(ForeignKey("Parent.id"))
+    __tablename__ = "child"
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String, unique = True)
+    parent = relationship("Parent", back_populates = "child")
+    parent_id = Column(Integer, ForeignKey("parent.id"))
+
+parent = Parent(name = "P")
+
+parent.child = [Child(name = "C")]
     
 opensession = Session()
 
-result = opensession.execute(select(Parent).order_by(Parent.id))
-print(result)
+print(parent.child[0].name)
+
+opensession.add(parent)
+opensession.commit()
 
 opensession.close()
